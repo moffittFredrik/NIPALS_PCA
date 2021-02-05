9798964d51b49260529774b5752ed1254373ffca
Fredrik Pettersson <fredrik.pettersson@moffitt.org>
2021-02-04 19:47:43 -0500

    disabled precompile test

--- src/precompiler/precompile_package.jl
+++ src/precompiler/precompile_package.jl
@@ -7,10 +7,10 @@ isnumcol(type) = type <: Union{Missing,Number}
 
 irisdir = @pipe pathof(NIPALS_PCA) |> splitpath |> _[1:end - 2] |> joinpath(_..., "test", "data", "iris")
 
-x_df = CSV.File(joinpath(irisdir, "iris.csv"), header=false) |> DataFrame
-select!(x_df, eltype.(eachcol(x_df)) |> coltypes -> isnumcol.(coltypes) |> findall)
-xdataset = parseDataFrame(x_df) |> dataset -> filterDataset(dataset, filters=[dataset -> dataset.mvs .< 0.25]) |> normalize
+#x_df = CSV.File(joinpath(irisdir, "iris.csv"), header=false) |> DataFrame
+#select!(x_df, eltype.(eachcol(x_df)) |> coltypes -> isnumcol.(coltypes) |> findall)
+#xdataset = parseDataFrame(x_df) |> dataset -> filterDataset(dataset, filters=[dataset -> dataset.mvs .< 0.25]) |> normalize
 
-pca = calcPCA(xdataset, 3)
+#pca = calcPCA(xdataset, 3)
 
-calcVariances(xdataset, pca)
+#calcVariances(xdataset, pca)
